46894f0a370bd7d3f9b44f508afcb3b119f0c8e2
Aryan Kumar Shrivastav <main.aryanshrivastav2003@gmail.com>
2025-07-21 11:43:11 +0530

    Adding Video Conferencing model

---
+++ models/videoRoom.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose')
+
+const videoRoomSchema=new mongoose.Schema({
+    tenantId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Tenant',
+        required : true
+    },
+    boardId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Whiteboard',
+        required : true
+    },
+    roomId : {
+        type : String,
+        unique : true,
+        required : true
+    },
+    createdBy : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    },
+    endedAt : {
+        type : Date
+    },
+    expiresAt : {
+        type : Date,
+        index : {
+            expires : 21600,                // 6 hours in seconds
+        }
+    }
+})
+
+
+module.exports = mongoose.model('VideoRoom', videoRoomSchema);
