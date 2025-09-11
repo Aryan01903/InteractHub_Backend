f0f3454350dee56b1d4129e749ffdff085c4f05d
Aryan Kumar Shrivastav <139858150+Aryan01903@users.noreply.github.com>
2025-09-11 23:51:39 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,7 @@ exports.userRegister = async (req, res) => {
 
         // Generate OTP
         const otp = Math.floor(100000 + Math.random() * 900000).toString();
-        const otpExpires = new Date(Date.now() + 10 * 60 * 1000); // 10 minutes from now
+        const otpExpires = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes from now
 
         // Check if user already exists
         let user = await User.findOne({ email: normalizedEmail });
