1c6410ee76e0c81c8b421f3265a12f1c0057d1cc
Aryan Kumar Shrivastav <main.aryanshrivastav2003@gmail.com>
2026-06-11 00:05:16 +0530

    changing send otp smtp to brevo

--- utils/sendOTP.js
+++ utils/sendOTP.js
@@ -1,62 +1,55 @@
-const { Resend } = require("resend");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
-console.log("Resend initialized");
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_MAIL,
+    pass: process.env.BREVO_SMTP_KEY,
+  },
+});
+
+const verifyTransporter = async () => {
+  try {
+    await transporter.verify();
+    console.log("Brevo SMTP Ready");
+  } catch (err) {
+    console.error("Brevo SMTP Error:", err);
+  }
+};
+
+verifyTransporter();
 
 const sendOtp = async (email, otp) => {
   console.log("Email:", email);
   console.log("OTP:", otp);
 
   try {
-    const response = await resend.emails.send({
-      from: "InteractHub <onboarding@resend.dev>",
+    const info = await transporter.sendMail({
+      from: `"InteractHub" <${process.env.MAIL_USER}>`,
       to: email,
       subject: "Your OTP for InteractHub Platform",
       html: `
         <div style="font-family: Arial, sans-serif; padding: 20px; background: #f9f9f9; border-radius: 10px;">
           <h2 style="color: #3AABB7;">OTP Verification</h2>
-
           <p>Dear User,</p>
-
-          <p>
-            Your verification code for
-            <strong>InteractHub</strong>
-            is:
-          </p>
-
-          <h1
-            style="
-              font-size:32px;
-              letter-spacing:5px;
-              color:#3AABB7;
-            "
-          >
+          <p>Your verification code for <strong>InteractHub</strong> is:</p>
+          <h1 style="font-size: 32px; letter-spacing: 5px; color: #3AABB7;">
             ${otp}
           </h1>
-
-          <p>
-            This OTP is valid for
-            <strong>5 minutes</strong>.
-          </p>
-
-          <p>
-            If you didn't request this,
-            please ignore this email.
-          </p>
-
-          <hr />
-
+          <p>This OTP is valid for <strong>5 minutes</strong>.</p>
+          <p>If you didn't request this, please ignore this email.</p>
+          <hr>
           <small>InteractHub Team</small>
         </div>
       `,
     });
 
-    console.log("OTP sent successfully");
-    console.log(response);
-
-    return response;
+    console.log("OTP sent:", info.messageId);
+    return info;
   } catch (err) {
     console.error("Failed to send OTP:", err);
     throw err;
