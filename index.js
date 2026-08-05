2436ac875901fbd92d22e4f458a358daa98954fa
Aryan Kumar Shrivastav <139858150+Aryan01903@users.noreply.github.com>
2026-08-05 21:54:53 +0530

    Change CORS origin from localhost:5173 to localhost:3000
    Updated CORS configuration to allow requests from localhost:3000 instead of localhost:5173.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const corsOptions = {
   origin: [
     "https://boardstack-pi.vercel.app",
     "https://interacthub.vercel.app",
-    "http://localhost:5173",
+    "http://localhost:3000",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
